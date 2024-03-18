b4c675198c2e0dbef95a026564588c8c5922c469
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-03-18 15:41:45 +0530

    Sort_Colors.java

--- Sort_Colors.java
+++ Sort_Colors.java
@@ -6,7 +6,4 @@ class Solution {
         while(curr<=end && start<end){//Iterating Nums array from start,current and ending [indx]
             if(nums[curr]==0){nums[curr]=nums[start];nums[start]=0;start++;curr++;}//swapping and increment current and start [indx] [0]values  
             else if(nums[curr]==2){nums[curr]=nums[end];nums[end]=2;end--;}//swapping and decrement current and start [indx] [2]values
-            else curr++;//incrementing the current indx val after sorting start and indx val to thier current sorted position
-        }
-    }
-}
+            else curr++;}}}//incrementing the current indx val after sorting start and indx val to thier current sorted position
